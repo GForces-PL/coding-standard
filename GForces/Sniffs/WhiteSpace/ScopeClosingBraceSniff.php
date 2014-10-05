6cab827b513f30674af01c35acd8ee7be35a815c
Michał Felski <fela@fela.pl>
2014-10-05 10:07:43 +0200

    scope closing brace sniff

---
+++ GForces/Sniffs/WhiteSpace/ScopeClosingBraceSniff.php
@@ -0,0 +1,45 @@
+<?php
+class GForces_Sniffs_WhiteSpace_ScopeClosingBraceSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     * @return array
+     */
+    public function register()
+    {
+        return PHP_CodeSniffer_Tokens::$scopeOpeners;
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     * @param PHP_CodeSniffer_File $phpcsFile All the tokens found in the document.
+     * @param int $stackPtr The position of the current token in the
+     *                                        stack passed in $tokens.
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        if (!isset($tokens[$stackPtr]['scope_closer'])) return;
+        $closeBrace = $tokens[$stackPtr]['scope_closer'];
+        $prevContent = $phpcsFile->findPrevious(T_WHITESPACE, ($closeBrace - 1), null, true);
+        if ($prevContent !== $tokens[$stackPtr]['scope_opener'] && $tokens[$prevContent]['line'] !== ($tokens[$closeBrace]['line'] - 1)) {
+            $error = 'The closing brace for the %s must go on the next line after the body';
+            $data = array($tokens[$stackPtr]['content']);
+            $fix = $phpcsFile->addFixableError($error, $closeBrace, 'CloseBraceAfterBody', $data);
+
+            if ($fix === true) {
+                $phpcsFile->fixer->beginChangeset();
+                for ($i = ($prevContent + 1); $i < $closeBrace; $i++) {
+                    $phpcsFile->fixer->replaceToken($i, '');
+                }
+
+                if (strpos($tokens[$prevContent]['content'], $phpcsFile->eolChar) === false) {
+                    $phpcsFile->fixer->replaceToken($closeBrace, $phpcsFile->eolChar . $tokens[$closeBrace]['content']);
+                }
+
+                $phpcsFile->fixer->endChangeset();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 NetBeans: http://plugins.netbeans.org/plugin/40282/phpmd-php-codesniffer-plugin
+
 PHPStorm: http://www.jetbrains.com/phpstorm/webhelp/using-php-code-sniffer-tool.html
