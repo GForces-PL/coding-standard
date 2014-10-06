0a62df29bd691d28d7c28abbb947f83cd4923d7c
Michał Felski <fela@fela.pl>
2014-10-06 08:13:30 +0200

    concatenation operator spacing

---
+++ GForces/Sniffs/WhiteSpace/StringOperatorSpacingSniff.php
@@ -0,0 +1,50 @@
+<?php
+
+class GForces_Sniffs_WhiteSpace_StringOperatorSpacingSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_STRING_CONCAT, T_CONCAT_EQUAL);
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token
+     *                                        in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        if ($tokens[$stackPtr + 1]['code'] != T_WHITESPACE || ($tokens[$stackPtr + 1]['code'] == T_WHITESPACE && $tokens[$stackPtr + 1]['length'] != 1)) {
+            $phpcsFile->addError('Expected one space after concatenation operator.', $stackPtr);
+        }
+
+        $position = $stackPtr - 1;
+        while ($tokens[$position]['code'] == T_WHITESPACE) {
+            $position--;
+        }
+
+        if ($position == $stackPtr - 1) {
+            $phpcsFile->addError('Missing space before concatenation operator', $stackPtr);
+            return;
+        }
+
+        if ($tokens[$stackPtr]['line'] - $tokens[$position]['line'] > 1) {
+            $phpcsFile->addError('Only one EOL character before concatenation operator is allowed.', $stackPtr);
+        }
+
+        if ($tokens[$stackPtr]['line'] == $tokens[$position]['line'] && $tokens[$stackPtr - 1]['length'] != 1) {
+            $phpcsFile->addError('Expected one space before concatenation operator.', $stackPtr);
+        }
+    }
+}
