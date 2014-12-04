348f31bd226245f62fd62107b806dc65f6c29e77
Michał Felski <fela@fela.pl>
2014-12-04 22:30:20 +0100

    disabled checking camel case class name for migration files

---
+++ GForces/Sniffs/Classes/ValidClassNameSniff.php
@@ -0,0 +1,9 @@
+<?php
+class GForces_Sniffs_Classes_ValidClassNameSniff extends Squiz_Sniffs_Classes_ValidClassNameSniff
+{
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        if (strpos($phpcsFile->getFilename(), '/protected/migrations/') !== false) return;
+        return parent::process($phpcsFile, $stackPtr);
+    }
+}
