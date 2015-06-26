c91d8871a88502ea21da6283dd59543d875afdd2
Michał Felski <fela@fela.pl>
2015-06-26 15:54:35 +0200

    omit spec_nightly

--- GForces/Sniffs/Methods/CamelCapsMethodNameSniff.php
+++ GForces/Sniffs/Methods/CamelCapsMethodNameSniff.php
@@ -15,7 +15,7 @@ class GForces_Sniffs_Methods_CamelCapsMethodNameSniff extends PSR1_Sniffs_Method
     {
         if ($namespace = $phpcsFile->findNext(T_NAMESPACE, 1)) {
             $tokens = $phpcsFile->getTokens();
-            if (isset($tokens[$namespace + 2]) && $tokens[$namespace + 2]['content'] == 'spec') {
+            if (isset($tokens[$namespace + 2]) && in_array($tokens[$namespace + 2]['content'], ['spec', 'spec_nightly'])) {
                 return;
             }
         }
